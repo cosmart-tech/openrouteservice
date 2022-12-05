ea8cec0eb901cd2b420b95c645a4f63a0eb4c67a
Ervan Dharmawan <ervandharmawanhalim@gmail.com>
2022-12-05 17:13:56 +0700

    change deploy scripts

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -14,7 +14,7 @@ echo New image name: $IMAGE_NAME_AND_TAG
 docker build --build-arg ENV_ARG=$BUILD_ENV . -t $IMAGE_NAME_AND_TAG
 
 ## push image to AWS
-IMAGE_LABEL=cosmart-ors-service-${GIT_REF}
-echo New image label: $IMAGE_LABEL
 
-aws lightsail push-container-image --region ap-southeast-1 --service-name ors-service --label $IMAGE_LABEL --image $IMAGE_NAME_AND_TAG
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 607133656658.dkr.ecr.ap-southeast-1.amazonaws.com
+docker tag $IMAGE_NAME_AND_TAG 607133656658.dkr.ecr.ap-southeast-1.amazonaws.com/cosmart-ors-service:latest
+docker push 607133656658.dkr.ecr.ap-southeast-1.amazonaws.com/cosmart-ors-service:latest
